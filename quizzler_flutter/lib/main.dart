64b230bfba74f18a047ace1183abad3b1538e4c6
amaanmajeed <amaanmajid123@gmail.com>
2022-08-23 13:43:55 +0500

    Added OOP in Quizzler app

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -1,8 +1,9 @@
-// ignore_for_file: deprecated_member_use, library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
-import 'package:quizzler_flutter/question.dart';
-import 'question.dart';
+// import 'package:quizzler_flutter/question.dart';
+// import 'question.dart';
+import 'quizbrain.dart';
 
 void main() => runApp(const Quizzler());
 
@@ -35,26 +36,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scorekeeper = [];
 
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.',
-  //   'My name is Khan'
-  // ];
-
-  // List<bool> ans = [false, true, true, false];
-
-  List<Question> questionbank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-    Question(q: 'My name is Khan', a: false),
-    Question(q: 'END', a: true)
-  ];
-
-  int questionNumber = 0;
+  QuizBrain quizBrain = QuizBrain();
 
   @override
   Widget build(BuildContext context) {
@@ -68,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionbank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   color: Colors.white,
@@ -95,14 +77,13 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    bool correctAnswer =
-                        questionbank[questionNumber].questionanswer;
+                    bool correctAnswer = quizBrain.getQuestionAnswer();
                     if (correctAnswer == true) {
                       scorekeeper.add(correctAns());
                     } else {
                       scorekeeper.add(wrongAns());
                     }
-                    questionNumber++;
+                    quizBrain.nextQuestion();
                   },
                 );
               },
@@ -126,14 +107,13 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    bool correctAnswer =
-                        questionbank[questionNumber].questionanswer;
+                    bool correctAnswer = quizBrain.getQuestionAnswer();
                     if (correctAnswer == false) {
                       scorekeeper.add(correctAns());
                     } else {
                       scorekeeper.add(wrongAns());
                     }
-                    questionNumber++;
+                    quizBrain.nextQuestion();
                   },
                 );
               },
--- quizzler_flutter/lib/question.dart
+++ quizzler_flutter/lib/question.dart
@@ -2,7 +2,7 @@ class Question {
   String questionText = '';
   bool questionanswer = true;
 
-  Question({required String q, required bool a}) {
+  Question(String q, bool a) {
     questionText = q;
     questionanswer = a;
   }
